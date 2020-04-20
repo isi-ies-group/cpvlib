ba1fddeb5beec61646ec473dc9ea4bda53142fb6
Ruben Nuñez <rubennj@users.noreply.github.com>
2020-04-20 23:58:13 +0200

    Update estimacion_temp_cell_difusa.py

--- ajuste_parametros/estimacion_temp_cell_difusa.py
+++ ajuste_parametros/estimacion_temp_cell_difusa.py
@@ -36,7 +36,10 @@ solar_azimuth = location.get_solarposition(data.index).azimuth
 data['aoi'] = pvlib.irradiance.aoi(surface_tilt=30, surface_azimuth=180,
                                    solar_zenith=solar_zenith, solar_azimuth=solar_azimuth)
 
-FF = 0.7
+FF = 0.5
+
+data['voc_si'] = (data.pmp_si / data.isc_si) / FF
+
 # https://doi.org/10.5281/zenodo.3346823
 # 0.1 m2, 572 (12x48) micro-cells + 4x 6"(15.24 cm) Si-cells in series
 # https://doi.org/10.5281/zenodo.3349781
@@ -44,23 +47,23 @@ FF = 0.7
 # Si rango IV: 1.7 A, 2.2 V @GNI 950 W/m2 y DNI/GNI=0.7
 
 data_low_aoi = data.query('aoi<55')
+vocr_low_aoi = data_low_aoi.query('990<gii<1010 and 19<temp_air<21 and wind_speed<2').voc_si.mean()
+iscr_low_aoi = data_low_aoi.query('990<gii<1010 and 19<temp_air<21 and wind_speed<2').isc_si.mean()
+
 data_upp_aoi = data.query('55<aoi<80')
-
-data_low_aoi['voc_si'] = (data_low_aoi.pmp_si / data_low_aoi.isc_si) / FF
-data_upp_aoi['voc_si'] = (data_upp_aoi.pmp_si / data_upp_aoi.isc_si) / FF
-
-data_low_aoi.query('990<gii<1010').voc_si.hist(bins=50, histtype='step', label='low aoi')
-data_upp_aoi.query('400<gii<500').voc_si.hist(bins=50, histtype='step', label='upp aoi')
+vocr_upp_aoi = data_upp_aoi.query('450<gii<550 and 19<temp_air<21 and wind_speed<2').voc_si.mean()
+iscr_upp_aoi = data_upp_aoi.query('450<gii<550 and 19<temp_air<21 and wind_speed<2').isc_si.mean()
 
 Ns = 4
 beta = -2.2e-3
 
-Vocr = 1.75
-data_low_aoi['tcell_si'] = 25 + (data_low_aoi.voc_si - Vocr)/(Ns*beta)
+k = 1.38064852e-23 # m2 kg s-2 K-1
+q = 1.60217662e-19 # coulombs
+
+data_low_aoi['tcell_si'] = (data_low_aoi.voc_si - vocr_low_aoi + Ns*beta*298) / (Ns*(1*k/q)*np.log(data_low_aoi.isc_si/iscr_low_aoi)+ Ns*beta) - 273
 ax=data_low_aoi.plot.scatter(x='temp_air', y='tcell_si', label='Si')
 data_low_aoi.plot.scatter(ax=ax, x='temp_air', y='temp_cell', color='r', label='III-V')
 
-Vocr = 1.85
-data_upp_aoi['tcell_si'] = 25 + (data_upp_aoi.voc_si - Vocr)/(Ns*beta)
+data_upp_aoi['tcell_si'] = (data_upp_aoi.voc_si - vocr_upp_aoi + Ns*beta*298) / (Ns*(1*k/q)*np.log(data_upp_aoi.isc_si/iscr_upp_aoi)+ Ns*beta) - 273
 ax=data_upp_aoi.plot.scatter(x='temp_air', y='tcell_si', label='Si')
 data_upp_aoi.plot.scatter(ax=ax, x='temp_air', y='temp_cell', color='r', label='III-V')
